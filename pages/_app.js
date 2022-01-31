9729d6ecc299947c746a407a4aca6f920b2972c2
Daniel Arango <arangomonsalve.daniel@gmail.com>
2022-01-30 21:07:24 -0500

    Login and main table

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import {ThirdwebWeb3Provider} from '@3rdweb/hooks'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const supportedChainIds = [4]
+const connectors = {
+  injected: {}
+}
+
+function MyApp({Component, pageProps}) {
+  return (
+    <ThirdwebWeb3Provider connectors={connectors} supportedChainIds={supportedChainIds}>
+      <Component {...pageProps} />
+    </ThirdwebWeb3Provider>)
 }
 
 export default MyApp
